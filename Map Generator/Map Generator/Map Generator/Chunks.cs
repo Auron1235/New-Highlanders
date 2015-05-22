a9690367e6b38bac77abd610c1a24f4c55539981
Auron1235 <auronultima@gmail.com>
2015-05-22 21:32:32 +0100

    Added the actually code

---
+++ Map Generator/Map Generator/Map Generator/Chunks.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Map_Generator
+{
+    public class Chunks
+    {
+        Random rand;
+        public Vector2 chunkPos;
+        public List<Rectangle> grassTiles;
+        public List<Rectangle> wallTiles;
+        List<Rectangle> grassImages;
+        List<Rectangle> wallImages;
+        int wallCount = 15;
+        int tileSize = 32;
+        Texture2D spriteSheet;
+        public Rectangle chunkBounds;
+        public bool playerPresent = false;
+
+        int width = 20;
+        int height = 20;
+
+        public void Initialize(Texture2D image, Vector2 pos)
+        {
+            rand = new Random();
+            chunkPos = pos;
+            spriteSheet = image;
+            chunkBounds = new Rectangle((int)pos.X, (int)pos.Y, tileSize * width, tileSize * height);
+
+            //Makes all tiles into floor as a base layer
+            grassTiles = new List<Rectangle>();
+            grassImages = new List<Rectangle>();
+
+            wallTiles = new List<Rectangle>();
+            wallImages = new List<Rectangle>();
+
+            CreateAChunk();
+        }
+
+        public void CreateAChunk()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    grassTiles.Add(new Rectangle((int)(x * tileSize + chunkPos.X), (int)(y * tileSize + chunkPos.Y), tileSize, tileSize));
+                }
+            }
+
+            //Randomly picks between the 4 floor tiles in the 
+            //spritesheet and gives all the grass tiles their images.
+            for (int i = 0; i < grassTiles.Count; i++)
+            {
+                grassImages.Add(new Rectangle(rand.Next(0, 4) * tileSize, 0, tileSize, tileSize));
+            }
+
+            //Picks random areas to be set as collidables and gives it a
+            //random wall pic to use.
+            for (int i = 0; i < wallCount; i++)
+            {
+                wallTiles.Add(new Rectangle(rand.Next(40, 600) + (int)chunkPos.X, rand.Next(40, 600) + (int)chunkPos.Y, tileSize, tileSize));
+                wallImages.Add(new Rectangle(rand.Next(0, 4) * tileSize, tileSize, tileSize, tileSize));
+            }
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            //Draws the background layer
+            for (int i = 0; i < grassTiles.Count; i++)
+            {
+                sb.Draw(spriteSheet, new Vector2(grassTiles[i].X, grassTiles[i].Y), grassImages[i], Color.White);
+            }
+            for (int i = 0; i < wallTiles.Count; i++)
+            {
+                sb.Draw(spriteSheet, new Vector2(wallTiles[i].X, wallTiles[i].Y), wallImages[i], Color.White);
+            }
+        }
+    }
+}
---
+++ Map Generator/Map Generator/Map Generator/Player.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Map_Generator
+{
+    class Player
+    {
+        public Vector2 position;
+        public Vector2 velocity;
+        public Rectangle bounds;
+        public Rectangle feet;
+        public Texture2D image;
+        public int health = 10;
+        public Vector2 previousPosition;
+        public Vector2 origin;
+
+        public Player(Vector2 position, Texture2D image)
+        {
+            this.position = position;
+            this.bounds = new Rectangle((int)position.X, (int)position.Y, 30, 40);
+            this.feet = new Rectangle((int)position.X, (int)position.Y + 10, 30, 30);
+            this.image = image;
+            velocity = Vector2.Zero;
+            origin = new Vector2(bounds.X + bounds.Width / 2, bounds.Y + bounds.Height / 2);
+        }
+
+        public void Collisions(List<Rectangle> walls)
+        {
+            for (int x = 0; x < walls.Count; x++)
+            {
+                Rectangle PreviousHitBox = new Rectangle((int)previousPosition.X, (int)previousPosition.Y + 10, feet.Width, feet.Height);
+
+                if (feet.Intersects(walls[x]))
+                {
+                    if (PreviousHitBox.Right <= walls[x].X)
+                    {
+                        position = previousPosition;
+                        feet = new Rectangle((int)position.X, (int)position.Y, feet.Width, feet.Height);
+                        velocity.X = 0;
+                        position.Y += velocity.Y;
+                    }
+                    if (PreviousHitBox.Left >= walls[x].X + 30)
+                    {
+                        position = previousPosition;
+                        feet = new Rectangle((int)position.X, (int)position.Y, feet.Width, feet.Height);
+                        velocity.X = 0;
+                        position.Y += velocity.Y;
+                    }
+                    if (PreviousHitBox.Bottom <= walls[x].Y)
+                    {
+                        position = previousPosition;
+                        feet = new Rectangle((int)position.X, (int)position.Y, feet.Width, feet.Height);
+                        velocity.Y = 0;
+                        position.X += velocity.X;
+                    }
+                    if (PreviousHitBox.Top >= walls[x].Y + 30)
+                    {
+                        position = previousPosition;
+                        feet = new Rectangle((int)position.X, (int)position.Y, feet.Width, feet.Height);
+                        velocity.Y = 0;
+                        position.X += velocity.X;
+                    }
+                }
+
+            }
+        }
+
+        public void Update(GameTime gameTime, KeyboardState key)
+        {
+            previousPosition = position;
+
+            if (key.IsKeyDown(Keys.W))
+            {
+                velocity.Y -= 0.2f;
+            }
+            if (key.IsKeyDown(Keys.S))
+            {
+                velocity.Y += 0.2f;
+            }
+            if (key.IsKeyDown(Keys.A))
+            {
+                velocity.X -= 0.2f;
+            }
+            if (key.IsKeyDown(Keys.D))
+            {
+                velocity.X += 0.2f;
+            }
+
+            if (velocity.X > 2)
+            {
+                velocity.X = 2;
+            }
+            if (velocity.Y > 2)
+            {
+                velocity.Y = 2;
+            }
+            if (velocity.X < -2)
+            {
+                velocity.X = -2;
+            }
+            if (velocity.Y < -2)
+            {
+                velocity.Y = -2;
+            }
+
+            position += velocity;
+
+            bounds.X = (int)position.X;
+            bounds.Y = (int)position.Y;
+            feet.X = (int)position.X;
+            feet.Y = (int)position.Y + 10;
+
+            velocity *= 0.98f;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(image, bounds, Color.White);
+        }
+    }
+}
