9474b3a4aecf5d59b1b756650170b7e6a39ba2bd
Gary Malcolmson <lothariopendragon@gmail.com>
2015-05-31 11:48:33 +0100

    Added Circle struct for collision detection

---
+++ Map Generator/Map Generator/Map Generator/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Map_Generator
+{
+    public struct Circle
+    {
+        public Vector2 Center { get; set; }
+        public float Radius { get; set; }
+
+        public Circle(Vector2 center, float radius)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public bool Contains(Vector2 point)
+        {
+            return ((point - Center).Length() <= Radius);
+        }
+
+        public bool Intersects(Circle other)
+        {
+            return ((other.Center - Center).Length() < (other.Radius - Radius));
+        }
+    }
+}
